f6776ddbf9dc98decf50ee41dd4a647b864dc26a
Vishesh jhadi <vishesh@gandhineru.com>
2024-07-30 18:12:41 +0530

    added components -- added

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,20 @@
 import { getProperties, getPropertyPolicies, getSinglePropertyBySlug } from "@/serverAction/PropertiesAPI";
+import DataRangePicker from "@/components/ui/DatePicker";
 import Image from "next/image";
+import Calender from "@/components/ui/calender";
+import PhoneInput from "@/components/PhoneInput";
 
 export default async function Home() {
 
   const singleProperty = await getPropertyPolicies("nextpax-demo-1");
-  console.log(singleProperty)
+
   return (
     <main>
       <section>
         <h1>Home page</h1>
+        <DataRangePicker />
+        <Calender />
+        <PhoneInput />  
       </section>
     </main>
   );
---
+++ components/ui/DatePicker.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import React from 'react';
+import { DatePicker } from 'antd';
+const { RangePicker } = DatePicker;
+
+const DataRangePicker = () => {
+  return <RangePicker />
+};
+
+export default DataRangePicker;
