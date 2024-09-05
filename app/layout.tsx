78680388b9a6c905a26457deb3b85844af00a10a
Vishesh jhadi <vishesh@gandhineru.com>
2024-09-05 18:13:15 +0530

    fixed fonts files -- fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,27 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import localFont from "next/font/local";
 import { AntdRegistry } from '@ant-design/nextjs-registry';
 import GlobalLayout from "@/components/GlobalLayout";
-const inter = Inter({ subsets: ["latin"] });
+
 
 
 const Switzer = localFont({
   src: [
     {
-      path: "../public/fonts/Switzer-Light.woff2",
+      path: "/fonts/Switzer-Light.woff2",
       weight: "300",
     },
     {
-      path: "../public/fonts/Switzer-Medium.woff2",
+      path: "/fonts/Switzer-Medium.woff2",
       weight: "500",
     },
     {
-      path: "../public/fonts/Switzer-Regular.woff2",
+      path: "/fonts/Switzer-Regular.woff2",
       weight: "400",
     },
     {
-      path: "../public/fonts/Switzer-Thin.woff2",
+      path: "/fonts/Switzer-Thin.woff2",
       weight: "100",
     },
     
@@ -32,7 +31,7 @@ const Switzer = localFont({
 const Span = localFont({
   src: [
     {
-      path: "../public/fonts/Span-Regular.otf",
+      path: "/fonts/Span-Regular.otf",
       weight: "400",
     },
     
