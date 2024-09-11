9f82473eb13e7a5189d87b7ceb9f815af919047c
Vishesh jhadi <vishesh@gandhineru.com>
2024-09-11 18:23:40 +0530

    added recommendeation list -- added

--- app/(pages)/properties/page.tsx
+++ app/(pages)/properties/page.tsx
@@ -1,13 +1,14 @@
 import PropertyFilterComponent from "@/components/Filter/PropertyFilters";
 import PropertyGrid from "@/components/PropertyComponents/PropertyGrid";
 import PropertyMap from "@/components/PropertyComponents/PropertyMap";
+import PropertySlider from "@/components/PropertyComponents/PropertySlider";
 import { getProperties } from "@/serverAction/PropertiesAPI";
 
 const PropertyListingPage = async () => {
   const getProperty = await getProperties({limit:5});
   return (
-      <div className="">
-        <div className="mb-8 border-t border-b border-gray py-5 flex justify-evenly">
+      <div className="space-y-8">
+        <section className="mb-8 border-t border-b border-gray py-5 flex justify-evenly">
           {/* Lets create a copy of below div using array method */}
           {Array(10).fill(0).map((_, index) => {
             return (
@@ -18,18 +19,19 @@ const PropertyListingPage = async () => {
             )
           })}         
          
-        </div>
-        <div className="flex flex-col lg:flex-row">
+        </section>
+        <section className="flex flex-col lg:flex-row">
           <div className="mb-6 w-full border border-gray rounded-xl p-4 lg:mb-0 lg:w-1/4">
             <PropertyFilterComponent />
           </div>
           <div className="w-full lg:w-full lg:pl-6">
             <PropertyGrid className="grid justify-between gap-5 gap-y-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3" PostPerPage={5} />
           </div>
-          <div>
-            {/* <PropertyMap properties={getProperty.properties} /> */}
-          </div>
-        </div>
+        </section>
+          <section className=" space-y-8">
+        <h2 className="">Our Recommendations</h2>
+        <PropertySlider className="mt-10" />
+      </section>
       </div>
 
   );
